fdf99e0df1e35f77fd6a70a6beeddea378e2fe3a
Paulo Gonçalves <paulogoncalves@recife.ifpe.edu.br>
2022-07-10 06:57:18 -0300

    Improvements in the color output in JS version

--- docs/GUI.js
+++ docs/GUI.js
@@ -1,78 +1,55 @@
 import Mastermind from "./Mastermind.js";
 import Winner from "./Winner.js";
 
-function GUI() {
-    let game;
-    let selectedColor = null;
-    let colors = ["blue", "red", "green", "yellow", "brown", "magenta", "orange", "silver"];
-    function init() {
+class GUI {
+    constructor() {
+        this.game = null;
+        this.selectedColor = null;
+        this.colors = ["blue", "red", "green", "yellow", "cyan", "magenta", "orange", "lime"];
+    }
+    init() {
         let s = document.querySelector("#numberOfCodes");
         let numberOfCodes = parseInt(s.value);
         s = document.querySelector("#numberOfColors");
         let numberOfColors = parseInt(s.value);
         s = document.querySelector("#numberOfTries");
         let numberOfTries = parseInt(s.value);
-        game = new Mastermind(numberOfColors, numberOfCodes, numberOfTries);
-        printBoard();
-        printColors();
-        setMessage("");
-        setActiveRow();
+        this.game = new Mastermind(numberOfColors, numberOfCodes, numberOfTries);
+        this.printBoard();
+        this.printColors();
+        this.setMessage("");
+        this.setActiveRow();
     }
-    function registerEvents() {
+    registerEvents() {
         let button = document.querySelector("#check");
-        button.onclick = check;
+        button.onclick = this.check.bind(this);
         button = document.querySelector("#start");
-        button.onclick = init;
-        init();
+        button.onclick = this.init;
+        this.init();
     }
-    function printBoard() {
-        let thead = document.querySelector("#board thead");
-        thead.innerHTML = "";
-
-        let tr = document.createElement("tr");
-        thead.appendChild(tr);
-
-        for (let i = 0; i <= game.getNumOfCodes(); i++) {
-            tr.appendChild(document.createElement("td"));
-        }
-
+    printBoard() {
         let tbody = document.querySelector("#board tbody");
         tbody.innerHTML = "";
 
-        for (let i = 0; i < game.getNumOfTries(); i++) {
-            tr = document.createElement("tr");
+        for (let i = 0; i < this.game.getNumOfTries(); i++) {
+            let tr = document.createElement("tr");
             tbody.appendChild(tr);
-            for (let j = 0; j < game.getNumOfCodes(); j++) {
+            for (let j = 0; j < this.game.getNumOfCodes(); j++) {
                 tr.appendChild(document.createElement("td"));
             }
-            let ltd = document.createElement("td");
-            tr.appendChild(ltd);
-
-            let table = document.createElement("table");
-            ltd.appendChild(table);
-
-            tr = document.createElement("tr");
-            table.appendChild(tr);
-
-            for (let k = 0; k < game.getNumOfCodes(); k++) {
-                if (k === Math.round(game.getNumOfCodes() / 2)) {
-                    tr = document.createElement("tr");
-                    table.appendChild(tr);
-                }
-                let td = document.createElement("td");
-                tr.appendChild(td);
-            }
+            tr.appendChild(document.createElement("td"));
+            tr.appendChild(document.createElement("td"));
         }
     }
-    function printColors() {
+    printColors() {
         let table = document.querySelector("#colors");
         table.innerHTML = "";
 
         let tr = document.createElement("tr");
         table.appendChild(tr);
 
-        for (let i = 0; i < game.getNumOfColors(); i++) {
-            if (i === Math.round(game.getNumOfColors() / 2)) {
+        for (let i = 0; i < this.game.getNumOfColors(); i++) {
+            if (i === Math.round(this.game.getNumOfColors() / 2)) {
                 tr = document.createElement("tr");
                 table.appendChild(tr);
             }
@@ -82,67 +59,55 @@ function GUI() {
 
         let tds = document.querySelectorAll("#colors td");
         tds.forEach((elem, index) => {
-            elem.style.backgroundColor = colors[index];
-            elem.onclick = setColor;
+            elem.style.backgroundColor = this.colors[index];
+            elem.onclick = this.setColor.bind(this);
         });
     }
-    function setColor(ev) {
-        selectedColor = ev.target.style.backgroundColor;
+    setColor(ev) {
+        this.selectedColor = ev.target.style.backgroundColor;
     }
-    function setRowColor(ev) {
-        ev.target.style.backgroundColor = selectedColor;
-        ev.target.style.borderColor = selectedColor;
+    setRowColor(ev) {
+        ev.target.style.backgroundColor = this.selectedColor;
+        ev.target.style.borderColor = this.selectedColor;
     }
-    function setMessage(text) {
+    setMessage(text) {
         let ret = document.querySelector("#message");
         ret.textContent = text;
     }
-    function setActiveRow() {
-        let cells = document.querySelectorAll(`#board > tbody > tr:not(:nth-child(${game.getNumOfTries()})) > td:not(:last-child)`);
-        cells.forEach(elem => elem.removeEventListener("click", setRowColor));
-        let cells2 = document.querySelectorAll(`#board > tbody > tr:nth-child(${game.getNumOfTries()}) > td:not(:last-child)`);
-        cells2.forEach(elem => elem.addEventListener("click", setRowColor));
+    setActiveRow() {
+        let cells = document.querySelectorAll(`#board > tbody > tr:not(:nth-child(${this.game.getNumOfTries()})) > td:not(:last-child)`);
+        cells.forEach(elem => elem.removeEventListener("click", this.setRowColor.bind(this)));
+        let cells2 = document.querySelectorAll(`#board > tbody > tr:nth-child(${this.game.getNumOfTries()}) > td:not(:last-child)`);
+        cells2.forEach(elem => elem.addEventListener("click", this.setRowColor.bind(this)));
     }
-    function check() {
-        let cells = document.querySelectorAll(`#board > tbody > tr:nth-child(${game.getNumOfTries()}) > td:not(:last-child)`);
+    check() {
+        let cells = document.querySelectorAll(`#board > tbody > tr:nth-child(${this.game.getNumOfTries()}) > td`);
         let result = [];
-        for (let cell of cells) {
-            let corDeFundo = cell.style.backgroundColor;
-            let colorIndex = colors.indexOf(corDeFundo);
+        for (let i = 0; i < this.game.getNumOfCodes(); i++) {
+            let corDeFundo = cells[i].style.backgroundColor;
+            let colorIndex = this.colors.indexOf(corDeFundo);
             result.push(colorIndex);
         }
         try {
-            let ret = game.play(result);
-            showHint(ret.getHint());
+            let ret = this.game.play(result);
+            this.showHint(ret.getHint());
             if (ret.getWinner() === Winner.WIN) {
-                showCode(ret.getCode());
-                setMessage("Game over. You win!");
+                this.setMessage("Game over. You win!");
             } else if (ret.getWinner() === Winner.LOSE) {
-                showCode(ret.getCode());
-                setMessage("Game over. You lose!");
+                this.setMessage("Game over. You lose!");
             } else {
-                setActiveRow();
+                this.setActiveRow();
             }
         } catch (ex) {
-            setMessage(ex.message);
+            this.setMessage(ex.message);
         }
     }
-    function showHint(result) {
-        let cells = document.querySelectorAll(`#board > tbody > tr:nth-child(${game.getNumOfTries() + 1}) > td:last-child td`);
-        let hint = ["white", "gray", "red"];
-        for (let i = 0; i < result.length; i++) {
-            cells[i].style.backgroundColor = hint[result[i]];
-            cells[i].style.borderColor = hint[result[i]];
-        }
+    showHint(result) {
+        let correctCell = document.querySelector(`#board > tbody > tr:nth-child(${this.game.getNumOfTries() + 1}) > td:nth-last-child(2)`);
+        correctCell.textContent = result.filter(n => n == 2).length;
+        let wrongCell = document.querySelector(`#board > tbody > tr:nth-child(${this.game.getNumOfTries() + 1}) > td:last-child`);
+        wrongCell.textContent = result.filter(n => n == 1).length;
     }
-    function showCode(code) {
-        let cells = document.querySelectorAll("#board thead td:not(:last-child)");
-        cells.forEach((elem, index) => {
-            elem.style.backgroundColor = colors[code[index]];
-            elem.style.borderColor = colors[code[index]];
-        });
-    }
-    return { registerEvents };
 }
 
 let gui = new GUI();
